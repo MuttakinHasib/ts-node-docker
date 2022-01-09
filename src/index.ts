ad7b58bcda66f57960f0aa5935789f816caf66c7
Hasib Molla <hasibmolla28@gmail.com>
2022-01-10 03:19:36 +0600

    added express and morgan :hammer::zap:

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-export const hello = () => console.log("Hello world!");
+import express from 'express';
+import morgan from 'morgan';
+
+const app = express();
+const port = Number(process.env.PORT ?? 5000);
+
+app.use(morgan('dev'));
+
+app.listen(port, '0.0.0.0', () => console.log('Server starter at', port));
